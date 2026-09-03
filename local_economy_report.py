2746a751b694defcc654aee036d51be5d1064845
kirakein-gif <kirakein@gmail.com>
2026-09-03 12:38:25 +0900

    Bump version for review data mapping improvements

--- local_economy_report.py
+++ local_economy_report.py
@@ -4,7 +4,7 @@ from ui_style import APP_CSS
 from app_mode1 import render_mode1
 from app_mode2 import render_mode2
 
-APP_VERSION = "1.3.0"
+APP_VERSION = "1.3.1"
 DEPLOY_DATE = "2026.09.03"
 
 st.set_page_config(

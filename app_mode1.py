b4d7bd63212f4398077a0a4cf5c265998f775bd3
kirakein-gif <kirakein@gmail.com>
2026-09-03 09:58:51 +0900

    Add mode 1 orchestrator

---
+++ app_mode1.py
@@ -0,0 +1,9 @@
+from mode1_prepare import prepare_mode1
+from mode1_address import render_address_tools
+from mode1_output import render_mode1_outputs
+
+
+def render_mode1():
+    ctx = prepare_mode1()
+    render_address_tools(ctx)
+    render_mode1_outputs(ctx)

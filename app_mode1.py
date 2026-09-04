61c99132b49059c89d08fd548ff55f6e738ebb70
kirakein-gif <kirakein@gmail.com>
2026-09-04 12:06:25 +0900

    Apply redesigned mode1 styles

--- app_mode1.py
+++ app_mode1.py
@@ -1,9 +1,13 @@
+import streamlit as st
+
 from mode1_prepare import prepare_mode1
 from mode1_address import render_address_tools
 from mode1_output import render_mode1_outputs
+from mode1_style import MODE1_CSS
 
 
 def render_mode1():
+    st.markdown(MODE1_CSS, unsafe_allow_html=True)
     ctx = prepare_mode1()
     render_address_tools(ctx)
     render_mode1_outputs(ctx)

77fc44cef7ffccd444e168b36f9fe070fd6c26c1
kirakein-gif <kirakein@gmail.com>
2026-09-16 15:52:17 +0900

    ui: load rich workflow styles

--- v2/frontend/src/main.jsx
+++ v2/frontend/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./styles.css";
 import "./final.css";
 import "./streamlit-theme.css";
+import "./workflow-rich.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>

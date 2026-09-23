b9a44e6db7b308511d4689dbeddc2be74a33ade5
kirakein-gif <kirakein@gmail.com>
2026-09-23 15:30:45 +0900

    ui: use consolidated v3 theme

--- v2/frontend/src/main.jsx
+++ v2/frontend/src/main.jsx
@@ -3,10 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./final.css";
-import "./streamlit-theme.css";
-import "./workflow-rich.css";
-import "./family-theme.css";
-import "./workflow-v2.css";
+import "./ui-v3.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
